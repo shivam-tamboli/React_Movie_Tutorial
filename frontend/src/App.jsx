bac02d72b5ddffb8307e0237bcf8871611d30a13
Shivam <shivamtamboli66@gmail.com>
2025-09-20 17:13:57 +0530

    Render Home component in App. Closes #4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 
 import './App.css'
-import MovieCard from './components/MovieCard'
+import Home from './pages/Home';
 
 function App() {
       return(
-        <>
-        <MovieCard  movie={{title: "Tim's Film", release_date: "2024" }}/>
-        <MovieCard  movie={{title: "Final destination", release_date: "2024" }}/>
-        </>
+       <>
+       <Home/>
+       </>
       );
   
 }
